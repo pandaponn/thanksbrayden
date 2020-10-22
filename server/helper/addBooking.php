f3eb580ca07b5d73738f0d16f0e99e1527028ed6
benk0811 <72331881+benk0811@users.noreply.github.com>
2020-10-22 14:03:10 +0800

    Added session into add booking

--- server/helper/addBooking.php
+++ server/helper/addBooking.php
@@ -1,10 +1,11 @@
 <?php
+    session_start();
     require_once "common.php";
     $dao = new InterviewDAO();
 
-    $result = $dao->createBooking('Gflhrz9j2G', 1, $_POST['radio_timeslot']);
+    $result = $dao->createBooking($_SESSION["id"], 1, $_POST['radio_timeslot']);
 
-    header("Location: ../../../../test/guugle-main/guugle/main_page/profile.php");
+    header("Location: ../../guugle-main/guugle/main_page/profile.php");
 
     // Add error message here if booking already exists
     return $result;
