f71721d3b8b67d7e14b841966ec551302d00f229
mangohsteen <72352643+mangohsteen@users.noreply.github.com>
2020-11-08 20:58:05 +0800

    Update result.php
    added condition if no results or searching with empty search (tried adding .box)

--- guugle-main/guugle/main_page/result.php
+++ guugle-main/guugle/main_page/result.php
@@ -46,20 +46,9 @@
     </nav>
 
     <!-- Search Results -->
+    <div class = 'box'>
     <div id="search_results" style="margin-top: 70px;">
-        <!-- <div class="fir-image-figure">
-            <a class="fir-imageover" rel="noopener" target="_blank" href="#">
-            <img class="fir-author-image fir-clickcircle" src="img/liyin.jpg">
-            </a>
-
-            <figcaption>
-            <div class="fig-author-figure-title">Ling Li Yin</div>
-            <div class="fig-author-figure-title">Engineer at Google</div>
-            <div class="fig-author-figure-title">Information Technology</div>
-            
-            </figcaption>
-            <button class="btn btn-dark btn-sm" style="margin-left: 15px;">Book now</button>
-        </div> -->
+    </div>
     </div>
 </body>
 </html>
@@ -69,96 +58,48 @@
     let search = '<?php echo $_POST['searchItem']; ?>'.toLowerCase();
     console.log(search);
 
+    var index = 0;
     function getInterviewers(){
     const request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if (this.readyState == 4 && this.status==200){
             let data = JSON.parse(this.responseText).interviewers;
             interviewers = data;
-        for (interviewer of data){
-            var industry = interviewer['industry'].toLowerCase();
-            var fname = interviewer['fname'].toLowerCase();
-            var lname = interviewer['lname'].toLowerCase();
-            if (fname.includes(search) || lname.includes(search) || industry.includes(search)){
+            for (interviewer of data){
+                var industry = interviewer['industry'].toLowerCase();
+                var fname = interviewer['fname'].toLowerCase();
+                var lname = interviewer['lname'].toLowerCase();
+                if ((fname.includes(search) || lname.includes(search) || industry.includes(search)) && search != ''){
 
-                var name = interviewer['fname'] + ' ' + interviewer['lname'];
-                var img = interviewer['img'];
-                var company = interviewer['company'];
-                var about = interviewer['about'];
-                var id = interviewer['id'];
-                var job = interviewer['job'];
-                var industry = interviewer['industry'];
+                    var name = interviewer['fname'] + ' ' + interviewer['lname'];
+                    var img = interviewer['img'];
+                    var company = interviewer['company'];
+                    var about = interviewer['about'];
+                    var id = interviewer['id'];
+                    var job = interviewer['job'];
+                    var industry = interviewer['industry'];
+                    index ++;
 
-                search_results.innerHTML += `
-                <div class="fir-image-figure">
-                <img class="fir-author-image fir-clickcircle" src="${img}">
+                    search_results.innerHTML += `
+                    <div class="fir-image-figure">
+                    <img class="fir-author-image fir-clickcircle" src="${img}">
 
-                <figcaption>
-                <div class="fig-author-figure-title">${name}</div>
-                <div class="fig-author-figure-title">${job}, ${company}</div>
-                <div class="fig-author-figure-title">${industry}</div>
-                </figcaption>
+                    <figcaption>
+                    <div class="fig-author-figure-title">${name}</div>
+                    <div class="fig-author-figure-title">${job}, ${company}</div>
+                    <div class="fig-author-figure-title">${industry}</div>
+                    </figcaption>
 
-                <form action="profile.php" method="POST">
-                <button class="btn btn-dark btn-sm" style="margin-left: 15px;" value="${id}" name="interviewer_id">Book now</button>
-                </form>
-                </div>
-                `;
-
-                // const card = document.createElement('div');
-                // const image = document.createElement('img');
-                // const cardBody = document.createElement('div');
-                // const cardTitle = document.createElement('h5');
-                // const cardTop = document.createElement('p');
-                // const cardBottom = document.createElement('p');
-                // const cardButton = document.createElement('button');
-
-                // card.className = 'card';
-
-                // image.className = 'card-img-top';
-                // image.src = img;
-
-                // cardBody.className = 'card-body';
-
-                // cardTitle.className = 'card-title';
-                // cardTitle.appendChild(document.createTextNode(name));
-
-                // cardTop.className = 'card-text font-weight-bold';
-
-                // cardTop.appendChild(document.createTextNode(company));
-
-                // cardBottom.className = 'card-text text-muted font-italic';
-                // cardBottom.appendChild(document.createTextNode(about));
-
-                // var form = document.createElement('form');
-                // form.method = 'POST';
-                // form.action = 'profile.php';
-
-                // var valueAttri = document.createAttribute('value');
-                // valueAttri.value = id;
-                // cardButton.className = 'btn btn-primary';
-                // cardButton.name = 'interviewer_id';
-                // cardButton.type = 'submit';
-                // cardButton.setAttributeNode(valueAttri);
-                // cardButton.appendChild(document.createTextNode('Book Now!'));
-
-                // form.appendChild(cardButton);
-
-                // cardBody.appendChild(cardTitle);
-                // cardBody.appendChild(cardTop);
-                // cardBody.appendChild(cardBottom);
-                // cardBody.appendChild(form);
-
-                // card.appendChild(image);
-                // card.appendChild(cardBody);
-
-                // document.getElementById('results').appendChild(card);
-            };
-        };
-        if (document.getElementById('search_results').innerHTML == ''){
-        alert("Sorry, we couldn't find the interviewer you were looking for.");
-        };
-            
+                    <form action="profile.php" method="POST">
+                    <button class="btn btn-dark btn-sm" style="margin-left: 15px;" value="${id}" name="interviewer_id">Book now</button>
+                    </form>
+                    </div>
+                    `;
+                }
+            }
+            if (index == 0){
+                alert('Unable to find what you are looking for.')
+            }
         }
     }
     request.open("GET", "../../../server/helper/getInterviewers.php", true);
@@ -167,5 +108,4 @@
     getInterviewers();
 
 </script>
-    
     
